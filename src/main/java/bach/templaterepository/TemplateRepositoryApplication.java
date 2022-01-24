d7d497361c8c74faf6843ff65b550de94cd9bcaf
Hoang Bach Le Le <lele100@uni-duesseldorf.de>
2022-01-24 18:08:40 +0100

    add Checkstyle, PMD, SpotBugs

--- src/main/java/bach/templaterepository/TemplateRepositoryApplication.java
+++ src/main/java/bach/templaterepository/TemplateRepositoryApplication.java
@@ -3,10 +3,11 @@ package bach.templaterepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SuppressWarnings("PMD.UseUtilityClass")
 @SpringBootApplication
 public class TemplateRepositoryApplication {
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         SpringApplication.run(TemplateRepositoryApplication.class, args);
     }
 
