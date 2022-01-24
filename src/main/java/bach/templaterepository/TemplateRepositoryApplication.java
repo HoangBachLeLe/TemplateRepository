b334f570c3d8c68d3f3ef51ebfd7a822fb0a905a
Hoang Bach Le Le <lele100@uni-duesseldorf.de>
2022-01-24 17:53:15 +0100

    skeleton project

---
+++ src/main/java/bach/templaterepository/TemplateRepositoryApplication.java
@@ -0,0 +1,13 @@
+package bach.templaterepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateRepositoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TemplateRepositoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bach/templaterepository/TemplateRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package bach.templaterepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateRepositoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
